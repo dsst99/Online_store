4fc21ecdeaa4dc8e7708d192ae0beb9e5edd779a
ds <it.dsjob@gmail.com>
2025-09-04 14:22:23 +0200

    Исправил Urls в приложении catalog в именах с _ на -

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -7,10 +7,10 @@ from .views import (
 
 urlpatterns = [
     # Категории
-    path('categories/', CategoryListView.as_view(), name="category_list"),
-    path('categories/<int:pk>/', CategoryView.as_view(), name="category_detail"),
+    path('categories/', CategoryListView.as_view(), name="category-list"),
+    path('categories/<int:pk>/', CategoryView.as_view(), name="category-detail"),
 
     # Продукты
-    path('products/', ProductListView.as_view(), name="product_list"),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name="product_detail"),
+    path('products/', ProductListView.as_view(), name="product-list"),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name="product-detail"),
 ]
