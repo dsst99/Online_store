7ae78a86f54230dd242559121f402647565883ea
ds <it.dsjob@gmail.com>
2025-09-06 18:12:23 +0200

    Подключил signals.py в apps.py

--- apps/orders/apps.py
+++ apps/orders/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class OrdersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.orders'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.orders"
+    verbose_name = "Заказы"
+
+    def ready(self):
+        from . import signals  # noqa: F401
